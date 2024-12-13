775497b21d7e51e741b0136eae54622d614ee2ea
DAMPFERS <vlad.razyvaev.2805@mail.ru>
2024-12-14 01:59:59 +0700

    added hard level stm32 programming and start risc-v work

--- stm32/STM32F103C/Hard_lv/main.c
+++ stm32/STM32F103C/Hard_lv/main.c
@@ -1,14 +1,13 @@
-#include <stm32f1xx.h>
+#include "stm32f1xx_hal_conf.h"
+#include <stdint.h>
 
+#include "stm32f1xx.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_rcc.h"
+#include "stm32f1xx_hal_gpio.h"
+#include  "stm32f1xx_hal_cec.h"
 
-void delay(uint32_t count){
-    while (count--)
-    {
-        __asm("NOP");
-    }
-    
-}
-
+void delay_RVV(uint32_t count);
 
 int main(void){
     // Включаем тактирование порта C
@@ -21,9 +20,15 @@ int main(void){
 
     while (1){
         GPIOC->ODR ^= GPIO_ODR_ODR13; // Инвертируем состояние пина PC13
-        delay(1000000);
+        delay_RVV(100000);
     }
-    
-    
+
     return 0;
+}
+
+void delay_RVV(uint32_t count){
+    while (count--)
+    {
+        __asm("NOP");
+    }
 }
